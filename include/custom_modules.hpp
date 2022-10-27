b06bc53b626943771864b23beb5961d0cee72dd9
EddyGlezz <eddyglezz95@gmail.com>
2022-10-27 15:26:55 +0200

    added gan modules

--- include/custom_modules.hpp
+++ include/custom_modules.hpp
@@ -1,6 +1,12 @@
 #ifdef USE_MPS
-#include<mps.hpp>
+#include"custom-modules/mps.hpp"
 #endif
 #ifdef USE_TTN
-#include<ttn.hpp>
+#include"custom-modules/ttn.hpp"
+#endif
+#ifdef USE_DGAN
+#include"custom-modules/dgan.hpp"
+#endif
+#ifdef USE_GGAN
+#include"custom-modules/ggan.hpp"
 #endif
